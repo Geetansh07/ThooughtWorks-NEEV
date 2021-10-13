8002da2f588b428e9fa6ff771846422a0304ef2e
Geetansh Kalra <geetansh.kalrathoughtworks.com@Geetanshs-MacBook-Pro.local>
2021-10-14 02:59:22 +0530

    some changes in the code of OOPS for making it better

--- trying_out_processing/src/main/java/ProcessingTest.java
+++ trying_out_processing/src/main/java/ProcessingTest.java
@@ -1,32 +1,45 @@
 import processing.core.PApplet;
 
-public class ProcessingTest extends PApplet {
+public class ProcessingTest extends PApplet{
 
-    public static final int WIDTH = 640;
-    public static final int HEIGHT = 480;
-    public static final int DIAMETER = 10;
+    int DIAMETER_OF_EllIPSE = 10;
+    int WIDTH = 640;
+    int HEIGHT = 480;
 
-    int x = 0;
+    int circle_1_speed = 1;
+    int circle_2_speed = 2;
+    int circle_3_speed = 3;
+    int circle_4_speed = 4;
 
-    public static void main(String[] args) { PApplet.main("ProcessingTest", args);
-    }
+    int circle_1_position = 1;
+    int circle_2_position = 2;
+    int circle_3_position = 3;
+    int circle_4_position = 4;
 
-    @Override
     public void settings() {
         size(WIDTH, HEIGHT);
     }
 
-    @Override
     public void draw() {
-        ellipse(x, HEIGHT / 5 , DIAMETER, DIAMETER);
 
-        ellipse(x * 2, HEIGHT * 2 / 5 , DIAMETER, DIAMETER);
-
-        ellipse(x * 3, HEIGHT * 3/ 5 , DIAMETER, DIAMETER);
-
-        ellipse(x * 4, HEIGHT * 4/ 5 , DIAMETER, DIAMETER);
-
-        x++;
+        making_ellipse(circle_1_speed, circle_1_position);
+        circle_1_speed += circle_1_position;
+        making_ellipse(circle_2_speed, circle_2_position);
+        circle_2_speed += circle_2_position;
+        making_ellipse(circle_3_speed, circle_3_position);
+        circle_3_speed += circle_3_position;
+        making_ellipse(circle_4_speed, circle_4_position);
+        circle_4_speed += circle_4_position;
 
     }
+
+    private void making_ellipse(int circle_speed, int circle_position) {
+        ellipse(circle_speed, circle_position*height/5, DIAMETER_OF_EllIPSE, DIAMETER_OF_EllIPSE);
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("ProcessingTest", args);
+
+    }
+
 }
